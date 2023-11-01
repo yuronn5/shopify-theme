d8459163fba53c269505bb96895128f9a1fa429c
yurii bilous <yurii.b@elogic.com.ua>
2023-11-01 16:15:41 +0200

    add hover for header

---
+++ assets/header-hover.js
@@ -0,0 +1,15 @@
+let items = document.querySelector(".header__inline-menu").querySelectorAll("header-menu > details");
+console.log(items)
+items.forEach(item => {
+    item.addEventListener("mouseover", () => {
+        item.setAttribute("open", true);
+
+        item.querySelector("ul").addEventListener("mouseleave", () => {
+            item.removeAttribute("open");
+        });
+
+        item.addEventListener("mouseleave", () => {
+            item.removeAttribute("open");
+        });
+    });
+});
